ed9640e6b4787ce88ee1bd2c7acc70aa875b1cb2
Joseph Brayan Smith Caicedo Sáenz <joseph.caice08@gmail.com>
2025-03-19 20:08:55 -0500

    feat: add functionality to generate test products file
    - Creates a method to generate a test products file (`createProductsFile`) with specified product count.
    - Implements basic exception handling for file I/O operations.

--- src/com/university/sales/generator/GenerateInfoFiles.java
+++ src/com/university/sales/generator/GenerateInfoFiles.java
@@ -1,14 +1,23 @@
 package com.university.sales.generator;
 
 import java.io.*;
+import java.util.Random;
 
 public class GenerateInfoFiles {
 
     private static final String SALES_DIR = "sales/";
+    private static final String PRODUCTS_FILE = "products.txt";
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        createDirectory(SALES_DIR);
-		System.out.println("Test files generated successfully.");
+        try {
+            createDirectory(SALES_DIR);
+            createProductsFile(10);
+
+            System.out.println("Test files generated successfully.");
+        } catch (IOException e) {
+            System.err.println("Error generating test files: " + e.getMessage());
+        }
     }
 
     private static void createDirectory(String dirPath) {
@@ -22,4 +31,12 @@ public class GenerateInfoFiles {
         }
     }
 
+
+    public static void createProductsFile(int productsCount) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PRODUCTS_FILE))) {
+            for (int i = 1; i <= productsCount; i++) {
+                writer.write(i + ";Product" + i + ";" + (RANDOM.nextInt(50) + 1) * 100 + "\n");
+            }
+        }
+    }
 }
